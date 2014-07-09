c5e5b089ca811998fa067ab5d1a271200b0620de
Dustin Liukkonen <dustin.liukkonen@superwarehouse.com>
2014-07-09 01:03:13 -0400

    Initial commit.

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+        "fmt"
+        "github.com/liukkonend/newmath"
+)
+
+func main() {
+        fmt.Printf("Hello, %s\n", "Dustin")
+        fmt.Printf("Sqr(5,3) = %v\n", newmath.Sqr(5,3))
+        fmt.Printf("Sqrt(2) = %v\n", newmath.Sqrt(2))
+}
